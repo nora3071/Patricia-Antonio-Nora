2c0736057d906f033062ad20dcc344bba6ac5778
nora3071 <nmobregon@uamv.edu.ni>
2025-09-24 07:26:15 -0600

    Add files via upload

--- NumeroPar/Program.cs
+++ NumeroPar/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static bool EsPar(int n)
+    {
+        return n % 2 == 0;
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese un número: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (EsPar(n))
+            Console.WriteLine("El número es par.");
+        else
+            Console.WriteLine("El número es impar.");
+    }
+}
+
